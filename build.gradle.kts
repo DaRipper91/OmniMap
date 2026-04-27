c916f2612e2f81b865e881bd9fb7ecda5d809e32
DaRipper91 <daripper91@users.noreply.github.com>
2026-04-27 18:20:28 -0500

    YOLO: Force Kotlin 2.0.21 and resolution strategy to kill version mismatch

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,19 @@
 plugins {
     id("com.android.application") version "8.3.2" apply false
     id("com.android.library") version "8.3.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.1.0" apply false
-    id("org.jetbrains.kotlin.multiplatform") version "2.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("org.jetbrains.kotlin.multiplatform") version "2.0.21" apply false
     id("org.jetbrains.compose") version "1.7.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.1.0" apply false
-    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.google.devtools.ksp") version "2.0.21-1.0.26" apply false
+}
+
+subprojects {
+    configurations.all {
+        resolutionStrategy.eachDependency {
+            if (requested.group == "org.jetbrains.kotlin") {
+                useVersion("2.0.21")
+            }
+        }
+    }
 }
